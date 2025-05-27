9c37a180fab4339ca8220a40508fb9a109b42503
MowlandCodes <mowlandgaming@gmail.com>
2025-05-27 15:09:05 +0700

    edit: cleaned dotfiles

---
+++ suckless/.dwm/autostart.sh
@@ -0,0 +1,5 @@
+dbus-launch &
+pulseaudio --start
+# Uncomment if you have picom installed and want a transparent background
+# picom &
+export PATH=$PATH:$HOME/.local/bin/ && dwmblocks 2>/tmp/dwmblocks.log &
