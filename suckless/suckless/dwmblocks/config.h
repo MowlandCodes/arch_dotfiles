95f3323ce99b7d74b4ff345e9ddc3105e416a306
MowlandCodes <mowlandgaming@gmail.com>
2025-05-27 20:05:24 +0700

    edit: using inversed background icon for dwmblocks & its scripts

--- suckless/suckless/dwmblocks/config.h
+++ suckless/suckless/dwmblocks/config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER " | "
+#define DELIMITER " "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 45
+#define MAX_BLOCK_OUTPUT_LENGTH 70
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 1
@@ -17,13 +17,13 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-#define BLOCKS(X)                                       \
-    X("", "sb-weather", 30, 5)                          \
-    X("^c#C6A0F6^ ^d^ ", "sb-mem", 10, 3)            \
-    X("", "sb-volume", 0, 12)                           \
-    X("", "sb-battery", 10, 11)                         \
-    X("", "sb-net", 5, 13)                              \
-    X("^c#FFBF12^󰃭 ^d^", "date '+%b %d, %Y'", 60, 1) \
-    X("^c#20ACFF^ ^d^", "sb-clock", 10, 1)
+#define BLOCKS(X)                                                     \
+    X("", "sb-weather", 30, 5)                                        \
+    X("^b#C6A0F6^^c#212233^   ^d^ ", "sb-mem", 10, 3)              \
+    X("", "sb-volume", 0, 12)                                         \
+    X("", "sb-battery", 10, 11)                                       \
+    X("", "sb-net", 5, 13)                                            \
+    X("^b#FFBF12^^c#212233^  󰃭  ^d^ ", "date '+%b %d, %Y'", 60, 1) \
+    X("^b#20ACFF^^c#212233^    ^d^ ", "sb-clock", 10, 1)
 
 #endif  // CONFIG_H
