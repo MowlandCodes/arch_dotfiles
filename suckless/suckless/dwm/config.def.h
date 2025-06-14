f111063c064854ccd969429ae95b36c35b604a1e
MowlandCodes <mowlandgaming@gmail.com>
2025-06-14 10:34:27 +0700

    edit: edit tag mask rule for few apps in dwm config

--- suckless/suckless/dwm/config.def.h
+++ suckless/suckless/dwm/config.def.h
@@ -592,15 +592,16 @@ static const Rule rules[] = {
         RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
             RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
                 RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
-                    RULE(.class = "Gimp", .tags = 1 << 4)
-                        RULE(.class = "Firefox", .tags = 1 << 7)
-                            RULE(.class = "xdman-Main", .isfloating = 1)
+                    RULE(.class = "xdman-Main", .isfloating = 1)
+                        RULE(.class = "pavucontrol", .isfloating = 1)
+                            RULE(.class = "ZapZap", .tags = 1 << 4)
+                                RULE(.class = "zen", .tags = 1 << 2)
 #if RENAMED_SCRATCHPADS_PATCH
-                                RULE(.instance = "spterm", .scratchkey = 's',
-                                     .isfloating = 1)
+                                    RULE(.instance = "spterm",
+                                         .scratchkey = 's', .isfloating = 1)
 #elif SCRATCHPADS_PATCH
-                                RULE(.instance = "spterm", .tags = SPTAG(0),
-                                     .isfloating = 1)
+                                    RULE(.instance = "spterm", .tags = SPTAG(0),
+                                         .isfloating = 1)
 #endif // SCRATCHPADS_PATCH
 };
 
