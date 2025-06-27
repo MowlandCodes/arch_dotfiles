79a5cdcf61ebb2658f905ddc0d97b9d065ad5b8e
MowlandCodes <mowlandgaming@gmail.com>
2025-06-27 10:29:23 +0700

    edit: use kitty image protocol for rendering image in nvim

--- nvim/.config/nvim/lua/plugins/image-nvim.lua
+++ nvim/.config/nvim/lua/plugins/image-nvim.lua
@@ -1,12 +1,20 @@
 return {
-    "3rd/image.nvim",
-    build = false,
-    opts = {},
-    config = function ()
-        local image = require("image")
-        image.setup({
-            backend = "ueberzug",
-            processor = "magick_cli",
-        })
-    end
+    {
+        "vhyrro/luarocks.nvim",
+        priority = 1001, -- this plugin needs to run before anything else
+        opts = {
+            rocks = { "magick" },
+        },
+    },
+    {
+        "3rd/image.nvim",
+        dependencies = { "luarocks.nvim" },
+        config = function()
+            require("image").setup({
+                backend = "kitty",
+                max_height_window_percentage = 50,
+                hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
+            })
+        end
+    }
 }
