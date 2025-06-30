0e74672f7e1b51959320948f1ac0af629d95b8f2
MowlandCodes <mowlandgaming@gmail.com>
2025-06-30 13:08:37 +0700

    add: added network statusbar module

--- suckless/suckless/dwmblocks/config.h
+++ suckless/suckless/dwmblocks/config.h
@@ -2,10 +2,10 @@
 #define CONFIG_H
 
 // String used to delimit block outputs in the status.
-#define DELIMITER " "
+#define DELIMITER "  "
 
 // Maximum number of Unicode characters that a block can output.
-#define MAX_BLOCK_OUTPUT_LENGTH 70
+#define MAX_BLOCK_OUTPUT_LENGTH 75
 
 // Control whether blocks are clickable.
 #define CLICKABLE_BLOCKS 1
@@ -17,15 +17,12 @@
 #define TRAILING_DELIMITER 0
 
 // Define blocks for the status feed as X(icon, cmd, interval, signal).
-// Keep the weather interval >= 30 seconds. Rate Limit issues.... LOL
-#define BLOCKS(X)                                          \
-    X("", "sb-weather", 30, 5)                             \
-    X("^b#C6A0F6^^c#212233^   ^d^ ", "sb-mem", 3, 3)    \
-    X("", "sb-volume", 0, 12)                              \
-    X("", "sb-battery", 5, 11)                             \
-    X("", "sb-net", 2, 13)                                 \
-    X("^b#FFBF12^^c#212233^  󰃭  ^d^ ",                  \
-      "echo \"^c#FFBF12^$(date '+%b %d, %Y')^d^\"", 60, 1) \
-    X("^b#20ACFF^^c#212233^    ^d^ ", "sb-clock", 10, 1)
+#define BLOCKS(X)             \
+    X("", "sb-net", 2, 3)     \
+    X("", "sb-mem", 3, 1)     \
+    X("", "sb-volume", 0, 12) \
+    X("", "sb-battery", 5, 2) \
+    X("", "sb-clock", 5, 2)   \
+    X("", "sb-date", 60, 2)
 
 #endif  // CONFIG_H
