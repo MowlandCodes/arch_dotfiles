57d91da81fb975c45f53bc9a0dce3b2accede65b
MowlandCodes <mowlandgaming@gmail.com>
2025-07-26 08:33:07 +0700

    edit: use chafa for image previewer

--- nvim/.config/nvim/lua/plugins/image-nvim.lua
+++ nvim/.config/nvim/lua/plugins/image-nvim.lua
@@ -1,20 +1,37 @@
 return {
-    {
-        "vhyrro/luarocks.nvim",
-        priority = 1001, -- this plugin needs to run before anything else
-        opts = {
-            rocks = { "magick" },
-        },
-    },
-    {
-        "3rd/image.nvim",
-        dependencies = { "luarocks.nvim" },
-        config = function()
-            require("image").setup({
-                backend = "kitty",
-                max_height_window_percentage = 50,
-                hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.svg" },
-            })
-        end
-    }
+	"princejoogie/chafa.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"m00qek/baleia.nvim",
+	},
+	config = function()
+		vim.g.baleia = require("baleia").setup({})
+
+		local chafa = require("chafa")
+		chafa.setup({
+			render = {
+				min_padding = 5,
+				show_label = true,
+			},
+			events = {
+				update_on_nvim_resize = true,
+			},
+		})
+
+		-- Command to colorize current buffer --
+		vim.api.nvim_create_user_command("BaleiaColorize", function()
+			vim.g.baleia.once(vim.api.nvim_get_current_buf())
+		end, { bang = true })
+
+		vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+			pattern = {
+				"*.md",
+				"*.html",
+				"*.mdx",
+			},
+			callback = function()
+				vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
+			end,
+		})
+	end,
 }
