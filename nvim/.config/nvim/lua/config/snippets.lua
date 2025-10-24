6aeac12c719301ba3198120ae3df05aeb48d836d
MowlandCodes <mowlandgaming@gmail.com>
2025-10-25 06:52:57 +0700

    added snippets for CompP python

--- nvim/.config/nvim/lua/config/snippets.lua
+++ nvim/.config/nvim/lua/config/snippets.lua
@@ -129,3 +129,43 @@ int main() {
 		)
 	),
 })
+
+ls.add_snippets("python", {
+	snip(
+		"cppy",
+		fmt(
+			[[
+import bisect
+import math
+import sys
+from bisect import bisect_left as bl
+from bisect import bisect_right as br
+from collections import defaultdict as dd
+from math import floor, gcd, log, sqrt
+from sys import stdin, stdout
+
+sys.setrecursionlimit(100000000)
+
+inp = lambda: int(input())
+strng = lambda: input().strip()
+jn = lambda x, l: x.join(map(str, l))
+strl = lambda: list(input().strip())
+mul = lambda: map(int, input().strip().split())
+mulf = lambda: map(float, input().strip().split())
+seq = lambda: list(map(int, input().strip().split()))
+
+ceil = lambda x: int(x) if (x == int(x)) else int(x) + 1
+ceildiv = lambda x, d: x // d if (x % d == 0) else x // d + 1
+
+flush = lambda: stdout.flush()
+stdstr = lambda: stdin.readline()
+stdint = lambda: int(stdin.readline())
+stdpr = lambda x: stdout.write(str(x))
+
+mod = 1000000007
+{}
+  ]],
+			{ ins(0) }
+		)
+	),
+})
