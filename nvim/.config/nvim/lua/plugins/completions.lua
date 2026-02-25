59673f7476fc92a6075166791cbe015d7bfd8677
MowlandCodes <mowlandcodes@aol.com>
2026-02-25 22:28:38 +0700

    change completion config

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -6,6 +6,7 @@ return {
 	},
 	{
 		"saghen/blink.cmp",
+		enabled = true,
 		dependencies = {
 			{
 				"Exafunction/codeium.nvim",
@@ -15,33 +16,71 @@ return {
 
 		version = "1.*",
 
-		---@module 'blink.cmp'
-		---@type blink.cmp.Config
 		opts = {
-			keymap = {
-				preset = "default",
-				["<Up>"] = { "select_prev", "fallback" },
-				["<Down>"] = { "select_next", "fallback" },
-				["<C-p>"] = false,
-				["<C-n>"] = false,
-				["<C-CR>"] = { "select_and_accept", "fallback" },
-				["<Tab>"] = { "select_and_accept", "fallback_to_mappings" },
-				["<C-e>"] = { "cancel" },
-				["<C-x>"] = { "show" },
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer", "codeium" },
+				providers = {
+					lsp = {
+						name = "lsp",
+						enabled = true,
+						module = "blink.cmp.sources.lsp",
+						score_offset = 1000,
+					},
+					luasnip = {
+						name = "luasnip",
+						enabled = true,
+						module = "blink.cmp.sources.luasnip",
+						score_offset = 950,
+					},
+					snippets = {
+						name = "snippets",
+						enabled = true,
+						module = "blink.cmp.sources.snippets",
+						score_offset = 900,
+					},
+					codeium = {
+						name = "Codeium",
+						module = "codeium.blink",
+						async = true,
+						score_offset = 1100,
+					},
+				},
 			},
 
+			keymap = {
+				preset = "default",
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+
+				["<Tab>"] = { "snippet_forward", "fallback" },
+				["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-p>"] = {},
+				["<C-n>"] = {},
+
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			},
 			appearance = {
 				nerd_font_variant = "mono",
 			},
 
-			completion = { documentation = { auto_show = true } },
-
-			sources = {
-				providers = {
-					codeium = { name = "Codeium", module = "codeium.blink", async = true },
+			completion = {
+				documentation = {
+					auto_show = true,
 				},
-				default = { "lsp", "path", "snippets", "buffer", "codeium" },
+				list = {
+					selection = {
+						preselect = false,
+					},
+				},
 			},
+
 			snippets = { preset = "luasnip" },
 
 			fuzzy = { implementation = "prefer_rust_with_warning" },
