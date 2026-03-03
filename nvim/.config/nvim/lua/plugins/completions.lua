2f04d53c31481bee2686c5b2b4468fb435906abd
MowlandCodes <mowlandcodes@aol.com>
2026-03-03 15:26:38 +0700

    using vscode style snippets for luasnip

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,90 +1,75 @@
 return {
-	{
-		"L3MON4D3/LuaSnip",
-		version = "2.*",
-		build = "make install_jsregexp",
+	"saghen/blink.cmp",
+	enabled = true,
+	dependencies = {
+		{
+			"Exafunction/codeium.nvim",
+			"rafamadriz/friendly-snippets",
+		},
 	},
-	{
-		"saghen/blink.cmp",
-		enabled = true,
-		dependencies = {
-			{
-				"Exafunction/codeium.nvim",
-				"rafamadriz/friendly-snippets",
+
+	version = "1.*",
+
+	opts = {
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer", "codeium" },
+			providers = {
+				lsp = {
+					name = "lsp",
+					enabled = true,
+					module = "blink.cmp.sources.lsp",
+					score_offset = 1000,
+				},
+				snippets = {
+					opts = {
+						friendly_snippets = true,
+					},
+				},
+				codeium = {
+					name = "Codeium",
+					module = "codeium.blink",
+					async = true,
+					score_offset = 1100,
+				},
 			},
 		},
 
-		version = "1.*",
+		keymap = {
+			preset = "default",
+			["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+			["<C-e>"] = { "hide", "fallback" },
+			["<CR>"] = { "accept", "fallback" },
 
-		opts = {
-			sources = {
-				default = { "lsp", "path", "snippets", "buffer", "codeium" },
-				providers = {
-					lsp = {
-						name = "lsp",
-						enabled = true,
-						module = "blink.cmp.sources.lsp",
-						score_offset = 1000,
-					},
-					luasnip = {
-						name = "luasnip",
-						enabled = true,
-						module = "blink.cmp.sources.luasnip",
-						score_offset = 950,
-					},
-					snippets = {
-						name = "snippets",
-						enabled = true,
-						module = "blink.cmp.sources.snippets",
-						score_offset = 900,
-					},
-					codeium = {
-						name = "Codeium",
-						module = "codeium.blink",
-						async = true,
-						score_offset = 1100,
-					},
-				},
-			},
+			["<Tab>"] = { "snippet_forward", "fallback" },
+			["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-			keymap = {
-				preset = "default",
-				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-				["<C-e>"] = { "hide", "fallback" },
-				["<CR>"] = { "accept", "fallback" },
+			["<Up>"] = { "select_prev", "fallback" },
+			["<Down>"] = { "select_next", "fallback" },
+			["<C-p>"] = {},
+			["<C-n>"] = {},
 
-				["<Tab>"] = { "snippet_forward", "fallback" },
-				["<S-Tab>"] = { "snippet_backward", "fallback" },
+			["<C-b>"] = { "scroll_documentation_up", "fallback" },
+			["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-				["<Up>"] = { "select_prev", "fallback" },
-				["<Down>"] = { "select_next", "fallback" },
-				["<C-p>"] = {},
-				["<C-n>"] = {},
-
-				["<C-b>"] = { "scroll_documentation_up", "fallback" },
-				["<C-f>"] = { "scroll_documentation_down", "fallback" },
-
-				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-			},
-			appearance = {
-				nerd_font_variant = "mono",
-			},
-
-			completion = {
-				documentation = {
-					auto_show = true,
-				},
-				list = {
-					selection = {
-						preselect = false,
-					},
-				},
-			},
-
-			snippets = { preset = "luasnip" },
-
-			fuzzy = { implementation = "prefer_rust_with_warning" },
+			["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
 		},
-		opts_extend = { "sources.default" },
+		appearance = {
+			nerd_font_variant = "mono",
+		},
+
+		completion = {
+			documentation = {
+				auto_show = true,
+			},
+			list = {
+				selection = {
+					preselect = false,
+				},
+			},
+		},
+
+		fuzzy = { implementation = "prefer_rust_with_warning" },
 	},
+
+	opts_extend = { "sources.default" },
 }
