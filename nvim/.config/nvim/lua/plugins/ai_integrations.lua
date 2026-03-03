9938604314d3bdbd3170bbec9482a5f4332d4b0a
MowlandCodes <mowlandcodes@aol.com>
2026-03-03 14:45:20 +0700

    fixed ai integrations with windsurf

--- nvim/.config/nvim/lua/plugins/ai_integrations.lua
+++ nvim/.config/nvim/lua/plugins/ai_integrations.lua
@@ -1,23 +1,21 @@
 return {
-	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({
-				enable_chat = true,
-				workspace_root = {
-					use_lsp = true,
-					find_root = function()
-						return vim.fn.getcwd()
-					end,
-				},
-			})
-
-			-- Spawning Codeium Chat in Browser (using formatted string)
-			vim.keymap.set("n", "<Leader>aa", ":Codeium Chat<CR>", { silent = true, desc = "Codeium Chat" })
-		end,
+	"Exafunction/windsurf.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
 	},
+	config = function()
+		require("codeium").setup({
+			enable_cmp_source = true,
+			virtual_text = {
+				enabled = true,
+			},
+			workspace_root = {
+				use_lsp = true,
+				find_root = function()
+					vim.fn.getcwd()
+				end,
+			},
+		})
+	end,
 }
