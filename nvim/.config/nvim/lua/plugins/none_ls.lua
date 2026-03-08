be8ce462d21ff4b09c7e4f41c83f7c6b5c5d5b55
MowlandCodes <mowlandcodes@aol.com>
2026-03-08 08:32:43 +0700

    fix hardcoded prettier config

--- nvim/.config/nvim/lua/plugins/none_ls.lua
+++ nvim/.config/nvim/lua/plugins/none_ls.lua
@@ -1,54 +1,12 @@
 return {
 	"nvimtools/none-ls.nvim",
 	dependencies = {
-		"MunifTanjim/prettier.nvim",
 		"gbprod/none-ls-ecs.nvim",
 	},
 	config = function()
 		local null_ls = require("null-ls")
-		local prettier = require("prettier")
-
 		local augroup = vim.api.nvim_create_augroup("lsp_format_on_save", { clear = false })
 		local event = "BufWritePre"
-
-		prettier.setup({
-			bin = "prettier",
-			filetypes = {
-				"css",
-				"javascript",
-				"javascriptreact",
-				"json",
-				"less",
-				"scss",
-				"typescript",
-				"typescriptreact",
-				"vue",
-				"yaml",
-				"html",
-				"astro",
-				"markdown",
-				"graphql",
-			},
-			cli_options = {
-				arrow_parens = "always",
-				bracket_spacing = true,
-				bracket_same_line = false,
-				embedded_language_formatting = "auto",
-				end_of_line = "lf",
-				html_whitespace_sensitivity = "css",
-				jsx_single_quote = false,
-				print_width = 120,
-				prose_wrap = "preserve",
-				quote_props = "as-needed",
-				semi = true,
-				single_attribute_per_line = false,
-				single_quote = false,
-				tab_width = 2,
-				trailing_comma = "es5",
-				use_tabs = false,
-				vue_indent_script_and_style = false,
-			},
-		})
 
 		null_ls.setup({
 			on_attach = function(client, bufnr)
@@ -78,7 +36,7 @@ return {
 				if client:supports_method("textDocument/rangeFormatting") then
 					vim.keymap.set("x", "<Leader>f", function()
 						vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
-					end, { buffer = bufnr, desc = "[lsp] format" })
+					end, { buffer = bufnr, desc = "[lsp] format range" })
 				end
 			end,
 
@@ -89,10 +47,24 @@ return {
 				null_ls.builtins.formatting.isort,
 				null_ls.builtins.code_actions.textlint,
 				null_ls.builtins.formatting.markdownlint,
+
 				null_ls.builtins.formatting.prettier.with({
-					extra_filetypes = { "ejs" },
-					extra_args = { "--trailing-comma", "none" },
+					extra_filetypes = {
+						"astro",
+						"blade",
+						"html",
+						"php",
+						"vue",
+						"javascript",
+						"typescript",
+						"json",
+						"bson",
+						"jsonc",
+						"typescriptreact",
+						"javascriptreact",
+					},
 				}),
+
 				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.formatting.pretty_php.with({ extra_args = { "--standard=PSR12" } }),
 				null_ls.builtins.diagnostics.djlint,
